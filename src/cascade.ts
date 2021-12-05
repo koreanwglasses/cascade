8350b7588e5ce7a506c534bb332a7481d3257e35
koreanwglasses <koreanwglasses@gmail.com>
2021-12-04 19:40:48 -0600

    feat: added managed and volatile

--- src/cascade.ts
+++ src/cascade.ts
@@ -7,60 +7,150 @@ type Whenever<T> = T | Promise<T>;
 type Listener = () => void;
 type ListenerHandle = { close(): void };
 
-export type Compute<S, T = undefined> = (
-  value: T,
-  /**
-   * Add a dependency on external state
-   */
-  addDependency: (...dependencies: Cascade[]) => void,
-  /**
-   * Wait until the next invalidate to re-compute
-   */
-  defer: () => void
-) => Whenever<S>;
-
-export class Cascade<T = any> {
+export class Volatile<T = any> {
   /**
    * Listeners from Cascade that are dependent on this
    */
   private listeners: Listener[] = [];
 
-  /**
-   * Handles for listening to Cascades this is dependent on
-   */
-  private handles: ListenerHandle[] = [];
-
-  private isValid = false;
-  private listen(listener: Listener): ListenerHandle {
+  protected listen(listener: Listener): ListenerHandle {
     this.listeners.push(listener);
     return {
       close: () => {
         const i = this.listeners.indexOf(listener);
         if (i !== -1) this.listeners.splice(i, 1);
 
-        if (this.listeners.length === 0) this.close?.();
+        if (this.listeners.length === 0) this.close();
       },
     };
   }
 
-  private notify() {
+  protected notify() {
     this.listeners.forEach((listener) => listener());
   }
 
-  private close() {
-    this.handles.forEach((sub) => sub.close());
+  protected close() {}
+
+  protected isValid = false;
+  protected curValue?: T;
+  protected curError?: any;
+
+  constructor() {
+    this.invalidate();
   }
 
-  private curValue?: T;
-  private curError?: any;
+  invalidate() {
+    this.isValid = false;
+  }
+
+  /**
+   * Chain the output of this Cascade into another computation
+   */
+  pipe<S>(compute: Compute<S, T>) {
+    const provider = this;
+    return new Cascade((_, deps, defer) => {
+      deps(provider);
+
+      if (!provider.isValid) throw defer();
+      if (provider.curError) throw provider.curError;
+      return compute(provider.curValue!, deps, defer);
+    });
+  }
+
+  p<S>(compute: Compute<S, T>) {
+    return this.pipe(compute);
+  }
+
+  /**
+   * Catch any errors thrown by an upstream Cascade
+   */
+  catch<S>(handler: (error: any) => Whenever<S>) {
+    const provider = this;
+    return new Cascade((_, deps, abort) => {
+      deps(provider);
+
+      if (!provider.isValid) throw abort();
+      if (provider.curError) return handler(provider.curError);
+      return provider.curValue!;
+    });
+  }
+
+  /**
+   * Chains a computation that returns a Cascade and outputs the nested type
+   */
+  join<S>(compute: Compute<Cascade<S>, T>) {
+    return this.pipe(
+      async (value, deps, abort) =>
+        await ((await compute(value, deps, abort)) as any).compute(
+          undefined,
+          deps,
+          abort
+        )
+    );
+  }
+
+  j<S>(compute: Compute<Cascade<S>, T>): Cascade<S> {
+    return this.join(compute);
+  }
+
+  next(): Promise<T> {
+    if (this.isValid) {
+      if (this.curError) return Promise.reject(this.curError);
+      return Promise.resolve(this.curValue!);
+    }
+
+    return new Promise((res, rej) => {
+      const handle = this.listen(() => {
+        if (this.curError) rej(this.curError);
+        else res(this.curValue!);
+
+        handle.close();
+      });
+    });
+  }
+}
+
+export class Managed<T = any> extends Volatile<T> {
+  value(value: T) {
+    this.curValue = value;
+    this.curError = null;
+    this.isValid = true;
+    this.notify();
+  }
+
+  error(error: any) {
+    this.curValue = undefined;
+    this.curError = error;
+    this.isValid = true;
+    this.notify();
+  }
+}
+
+export type Compute<S, T = undefined> = (
+  value: T,
+  /**
+   * Add a dependency on external state
+   */
+  addDependency: (...dependencies: Volatile[]) => void,
+  /**
+   * Wait until the next invalidate to re-compute
+   */
+  defer: () => void
+) => Whenever<S>;
+
+export class Cascade<T = any> extends Volatile<T> {
+  /**
+   * Handles for listening to Cascades this is dependent on
+   */
+  private handles: ListenerHandle[] = [];
 
   private prevValue?: T;
   private prevError?: any;
   async invalidate() {
     this.isValid = false;
 
-    let deps: Cascade[] = [];
-    const addDeps = (...deps_: Cascade[]) => deps.push(...deps_);
+    let deps: Volatile[] = [];
+    const addDeps = (...deps_: Volatile[]) => deps.push(...deps_);
 
     const abort = () => {
       throw ABORT;
@@ -90,13 +180,17 @@ export class Cascade<T = any> {
     this.setDeps(deps);
   }
 
-  private setDeps(deps: Cascade<any>[]) {
+  private setDeps(deps: Volatile[]) {
     const handles = [...new Set(deps)].map((dep) =>
-      dep.listen(() => this.invalidate())
+      (dep as Cascade).listen(() => this.invalidate())
     );
 
     this.handles.forEach((handle) => handle.close());
     this.handles = handles;
+  }
+
+  protected close() {
+    this.handles.forEach((sub) => sub.close());
   }
 
   /**
@@ -110,71 +204,7 @@ export class Cascade<T = any> {
    * @param compute The expression to be evaluated
    */
   constructor(private compute: Compute<T>) {
-    this.invalidate();
-  }
-
-  /**
-   * Chain the output of this Cascade into another computation
-   */
-  pipe<S>(compute: Compute<S, T>) {
-    const provider = this;
-    return new Cascade((_, deps, abort) => {
-      deps(provider);
-
-      if (!provider.isValid) throw abort();
-      if (provider.curError) throw provider.curError;
-      return compute(provider.curValue!, deps, abort);
-    });
-  }
-
-  p<S>(compute: Compute<S, T>) {
-    return this.pipe(compute);
-  }
-
-  /**
-   * Catch any errors thrown by an upstream Cascade
-   */
-  catch<S>(handler: (error: any) => Whenever<S>) {
-    const provider = this;
-    return new Cascade((_, deps, abort) => {
-      deps(provider);
-
-      if (!provider.isValid) throw abort();
-      if (provider.curError) return handler(provider.curError);
-      return provider.curValue!;
-    });
-  }
-
-  /**
-   * Chains a computation that returns a Cascade and outputs the nested type
-   */
-  join<S>(compute: Compute<Cascade<S>, T>) {
-    return this.pipe(
-      async (value, deps, abort) =>
-        await (
-          await compute(value, deps, abort)
-        ).compute(undefined, deps, abort)
-    );
-  }
-
-  j<S>(compute: Compute<Cascade<S>, T>): Cascade<S> {
-    return this.join(compute);
-  }
-
-  next(): Promise<T> {
-    if (this.isValid) {
-      if (this.curError) return Promise.reject(this.curError);
-      return Promise.resolve(this.curValue!);
-    }
-
-    return new Promise((res, rej) => {
-      const handle = this.listen(() => {
-        if (this.curError) rej(this.curError);
-        else res(this.curValue!);
-
-        handle.close();
-      });
-    });
+    super();
   }
 
   /**

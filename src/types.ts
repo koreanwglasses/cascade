2e728ed60018c3094ecbd10013632b68b2b83174
koreanwglasses <koreanwglasses@gmail.com>
2021-12-10 12:48:11 -0600

    fix: updated types to work with promises

--- src/types.ts
+++ src/types.ts
@@ -10,8 +10,8 @@ export type Nested<T = unknown> = T | Volatile<Nested<T>>;
 export type BaseType<T> = T extends Volatile<infer S> ? BaseType<S> : T;
 export type ArrayBaseType<T> = T extends readonly []
   ? readonly []
-  : T extends readonly [infer U, ...infer V]
+  : T extends readonly [Whenever<infer U>, ...infer V]
   ? readonly [BaseType<U>, ...ArrayBaseType<V>]
-  : T extends readonly (infer U)[]
+  : T extends readonly (Whenever<infer U>)[]
   ? readonly BaseType<U>[]
   : never;

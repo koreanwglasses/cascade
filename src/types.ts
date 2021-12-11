7ba321ec43712ff1b05b4451aef42f2f4f4e4aea
koreanwglasses <koreanwglasses@gmail.com>
2021-12-11 12:50:38 -0600

    fix: types

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,7 @@ export type ListenerHandle = { close(): void };
 
 export type Nested<T = unknown> = T | Volatile<Nested<T>>;
 
-export type BaseType<T> = Awaited<T> extends Volatile<infer S> ? BaseType<S> : T;
+export type BaseType<T> = T extends Volatile<infer S> ? BaseType<S> : T;
 
 export type Compute<S, T = undefined> = (
   value: T,
@@ -26,8 +26,8 @@ export type $Compute<S, T = {}> = (
 ) => Whenever<S>;
 
 export type $Flat<T> = T extends $<infer S>
-  ? { [K in keyof S]: BaseType<S[K]> }
-  : { [K in keyof T]: BaseType<T[K]> };
+  ? { [K in keyof S]: BaseType<Awaited<S[K]>> }
+  : { [K in keyof T]: BaseType<Awaited<T[K]>> };
 
 export type $Unpacked<S> = S extends $<infer T> ? T : never;
 
